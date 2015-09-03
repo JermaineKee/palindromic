ebf443d8be8eb5905ebf47edc4a783282390faf9
Jermaine Kee <Jarvis@Jermaines-MacBook-Pro.local>
2015-09-02 21:42:30 -0400

    palindrome normal code

--- palindrome.py
+++ palindrome.py
@@ -1,12 +1,40 @@
+import re
+
 def is_palindrome(sentence):
-    # TODO: return True or False if the sentence is or isn't a palindrome
-    pass
+    sentence = re.sub(r'[^A-Za-z]', "", (sentence.lower()))
+
+
+    min = 0
+    max = len(sentence) - 1
+    #scan sentence for equality
+    while True:
+        #all characters checked??? return true
+        if min > max:
+            return True;
+
+        a = sentence[min]
+        b = sentence[max]
+
+        #if they are not equal, return False
+        if a != b:
+            return False;
+
+        #move to the middle from both ends
+        min += 1
+        max -= 1
+
+
 
 
 def main():
-    # TODO: put your input/output code here
-    pass
-
+    #ask for sentence
+    sentence = input("Please give me a sentence you think is a palindrome ")
+    is_palindrome(sentence)
+    check = is_palindrome(sentence)
+    if check:
+        print("{} is a palindrome".format(sentence))
+    else:
+        print("{} is not a palindrome".format(sentence))
 
 if __name__ == '__main__':
     main()
